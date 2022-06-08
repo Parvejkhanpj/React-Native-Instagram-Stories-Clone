aae7b67b26b1b81bfe8bc61811556a6d400b537c
Parvej khan <97285732+Parvejkhanpj@users.noreply.github.com>
2022-06-08 23:26:50 +0530

    update commit

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,23 +1,23 @@
 // @flow
 import * as React from 'react';
 import {
-  StyleSheet, View, Image, Text, Platform,
+  StyleSheet, View, Image, Text, Platform,    // platform is a use when we update our app on multiple platfrom 
 } from 'react-native';
-import { Constants } from 'expo';
-import type { ImageSourcePropType } from 'react-native/Libraries/Image/ImageSourcePropType';
+import { Constants } from 'expo';              //contants usse for device details ex. cureent heigh
+import type { ImageSourcePropType } from 'react-native/Libraries/Image/ImageSourcePropType';   // normal prop of image 
 
-type AvatarProps = {
+type AvatarProps = {                                                // define the prop of avtar is string and image is a proptype 
   user: string,
   avatar: ImageSourcePropType,
 };
-
-export default class Avatar extends React.PureComponent<AvatarProps> {
+ 
+export default class Avatar extends React.PureComponent<AvatarProps> {                        //define the class component for avtar 
   render(): React.Node {
-    const { user, avatar: source } = this.props;
+    const { user, avatar: source } = this.props;                   // props 
     return (
-      <View style={styles.container}>
-        <Image {...{ source }} style={styles.avatar} />
-        <Text style={styles.username}>{user}</Text>
+      <View style={styles.container}>                        // make a view view container under view create image and text component 
+        <Image {...{ source }} style={styles.avatar} />     // source definee use a pros
+        <Text style={styles.username}>{user}</Text>          
       </View>
     );
   }
